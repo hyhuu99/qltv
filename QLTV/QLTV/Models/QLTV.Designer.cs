e647ccf9dcba1c4aef2a3cada1a7699659be87b1
Khanh Nguyen <30751848+hyhuu99@users.noreply.github.com>
2017-12-23 16:57:53 +0700

    last commit

--- QLTV/QLTV/Models/QLTV.Designer.cs
+++ QLTV/QLTV/Models/QLTV.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\project\github\qltv\QLTV\QLTV\Models\QLTV.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\project\qltv\QLTV\QLTV\Models\QLTV.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
